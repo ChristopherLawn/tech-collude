aa14b209ce3b3a25b63dbfe6fbf06e9771da0ed0
CLAW <christopher.lawn@yahoo.com>
2022-02-11 01:07:14 -0800

    updated models/index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
 module.exports = { User, Post };
